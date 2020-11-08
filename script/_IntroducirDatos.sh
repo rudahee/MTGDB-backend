9513ab83b5dbac7fed88a8fa2abff73b1511776b
Ruben <rudahee@gmail.com>
2020-11-08 23:43:29 +0100

    actualizado script de insercion de datos

--- script/_IntroducirDatos.sh
+++ script/_IntroducirDatos.sh
@@ -2,12 +2,12 @@ curl -d "@cards.json" -H "Content-Type: application/json" -X POST http://localho
 curl -d "@sets.json" -H "Content-Type: application/json" -X POST http://localhost:8081/mtgdb/sets
 curl -d "@formats.json" -H "Content-Type: application/json" -X POST http://localhost:8081/mtgdb/formats
 
-for i in {1..13}
+for i in {1..15}
 do
     curl -d "@Relationships/CardsToSets/$i.json" -H "Content-Type: application/json" -X PUT http://localhost:8081/mtgdb/cards/$i
 done
 
-for j in {14..17}
+for j in {16..19}
 do
     curl -d "@Relationships/SetsToFormats/$j.json" -H "Content-Type: application/json" -X PUT http://localhost:8081/mtgdb/format/$j
 done
